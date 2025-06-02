d2d1871f20936633230a4d12709cd5135bf4bdd9
Yarce22 <alejomira96@gmail.com>
2025-06-02 13:24:04 -0500

    cambios en el middleware para la descargar del cv

--- src/middleware.ts
+++ src/middleware.ts
@@ -40,6 +40,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)'
+    '/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|.*\.[^/]+$).*)'
   ],
 };
