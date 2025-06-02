0b01a78520a42572afee8d4782e2fd7c4dc3ce53
Yarce22 <alejomira96@gmail.com>
2025-06-02 14:45:20 -0500

    correccion estilos componente Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,7 +7,7 @@ interface ButtonProp {
 const Button: React.FC<ButtonProp> = ({ children, type = "button", disabled }) => {
 
   return (
-    <button className="relative max-w-80 h-14 px-10 bg-Titles rounded-xl cursor-pointer font-Geist font-bold text-Text tracking-[1.5px] after:absolute after:content-[''] after:inset-0 after:z-0 after:bg-white after:opacity-0 after:transition after:duration-400 after:ease-in-out hover:after:opacity-20" type={type} disabled={disabled}>{ children }</button>
+    <button className="relative max-w-80 h-14 px-10 bg-Titles rounded-xl cursor-pointer font-Geist font-bold text-Text tracking-[1.5px] after:absolute after:content-[''] after:inset-0 after:z-0 after:bg-white after:rounded-xl after:opacity-0 after:transition after:duration-400 after:ease-in-out hover:after:opacity-20" type={type} disabled={disabled}>{ children }</button>
   )
 }
 
